e2788e621e5628324c88c5161475cbe0701c6201
Yehuda Katz <yakatz@users.noreply.github.com>
2014-10-29 00:48:57 -0400

    Throw error for missing provider
    This allows supplying the provider in a form and easily catching the error

--- src/Artdarek/OAuth/OAuth.php
+++ src/Artdarek/OAuth/OAuth.php
@@ -68,7 +68,7 @@ class OAuth
     public function setConfig( $service )
     {
         // if config/oauth-4-laravel.php exists use this one
-        if ( Config::get('oauth-4-laravel.consumers') != null ) {
+        if ( Config::get('oauth-4-laravel.consumers.$service') != null ) {
 
             $this->_storage_name = Config::get('oauth-4-laravel.storage', 'Session');
             $this->_client_id = Config::get("oauth-4-laravel.consumers.$service.client_id");
@@ -76,11 +76,13 @@ class OAuth
             $this->_scope = Config::get("oauth-4-laravel.consumers.$service.scope", array() );
 
         // esle try to find config in packages configs
-        } else {
+        } elseif ( Config::get('oauth-4-laravel::consumers.$service') != null ) {
             $this->_storage_name = Config::get('oauth-4-laravel::storage', 'Session');
             $this->_client_id = Config::get("oauth-4-laravel::consumers.$service.client_id");
             $this->_client_secret = Config::get("oauth-4-laravel::consumers.$service.client_secret");
             $this->_scope = Config::get("oauth-4-laravel::consumers.$service.scope", array() );
+        } else {
+            throw new Exception("Unknown OAuth Provider");
         }
     }
 
